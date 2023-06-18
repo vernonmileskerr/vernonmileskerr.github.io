851c4e51d596f2fa2b182af8061c5e8de90f2371
VMK Blog Support <150648120+vmk-press@users.noreply.github.com>
2023-06-18 18:03:17 +0900

    Add jekyll-include-cache
    See: jekyll-theme-chirpy/issues/1094

---
+++ tools/theme-overrides-diff.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Only expecting small single line tweaks. If a large diff is displayed the theme
+was probably updated. Consider updating the theme and then re-applying the overrides."
+
+# Find the root directory of the current git repository
+root_dir=$(git rev-parse --show-toplevel)
+jekyll_dir=$(bundle info --path jekyll-theme-chirpy)
+cd "${root_dir}"
+
+# Find all directories in the root directory
+for dir in $(find ${jekyll_dir} -type d -depth 1); do
+
+  # Compute the relative path of the directory
+  rel_dir=$(realpath $dir | sed "s|${jekyll_dir}/||")
+  echo "Checking ${rel_dir}..."
+
+  # Run git diff for each relevant directory
+  if [[ -d "$root_dir/$rel_dir" ]]; then
+    git diff --no-index --diff-filter=M $jekyll_dir/$rel_dir $rel_dir &2>1 > /dev/null
+  else
+    echo "skipped ${rel_dir} (no override)."
+  fi
+done
